05c13e3e138619ff29ced13cac00135ef08894d2
nehuen233 <nehuenhugobarrios@gmail.com>
2024-06-04 11:17:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 function evento(e){
     console.dir(e.target.children[0]);
-    let h2 = e.target.children[0];
+    let hijo = e.target.children;
+    let clase = e.target;
+    clase.className = "boxActivo";
     
+    for (let i = 0; i < hijo.length; i++) {
+        if (hijo[i].id != e.target.id) {
+            hijo[i].className = "box";
+        }
+        
+    }
 
 }
